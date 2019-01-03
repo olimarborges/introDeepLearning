bb5f74d3c134c4d335a9eba1158be8be9e56d545
olimarborges <olimar.borges@acad.pucrs.br>
2019-01-03 18:27:12 -0200

    Segunda versão. Ainda em construção. Imagens separadas de maneira randômica entre train e test, onde seleciona 80% de cada classe de carro para o train e 20% de cada classe para o test, por script, nada manual. Dia 03/01/2018 18:24

--- pre-process_new.py
+++ pre-process_new.py
@@ -14,80 +14,102 @@ def ensure_folder(folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
 
+def find_paths(path):
+    paths = []
+    level_a = os.listdir(path)
+    #print level_a
+    for level_name in level_a:
+    #   print level_name
+        for image_name in os.listdir(os.path.join(path, level_name)):
+            paths += [os.path.join(path, level_name, image_name)]
 
-def save_train_data(fnames):
-    src_folder = 'cars_train'
+    return paths
+
+def save_train_and_test_data(fnames, fnamesPaste, dict_labels):
+
+    src_folder = 'data/DeepLearningFilesPosAug'
     num_samples = len(fnames)
-    print 'num_samples: ', num_samples
+    print 'num_samples: ', num_samples #1903
 
     train_split = 0.8
     num_train = int(round(num_samples * train_split))
-    print 'num_train: ', num_train
-    
+    print 'num_train: ', num_train #1522
+
     train_indexes = random.sample(range(num_samples), num_train)
     print 'train_indexes: ', train_indexes
 
-    pb = ProgressBar(total=100, prefix='Save train data', suffix='', decimals=3, length=50, fill='=')
-
-    # for i in range(num_samples):
-    #     fname = fnames[i]
-    #     #label = labels[i]
-    #     #(x1, y1, x2, y2) = bboxes[i]
-
-    #     src_path = os.path.join(src_folder, fname)
-    #     src_image = cv.imread(src_path)
-    #     #height, width = src_image.shape[:2]
-
-    #     # margins of 16 pixels
-    #     # margin = 16
-    #     # x1 = max(0, x1 - margin)
-    #     # y1 = max(0, y1 - margin)
-    #     # x2 = min(x2 + margin, width)
-    #     # y2 = min(y2 + margin, height)
-    #     # print("{} -> {}".format(fname, label))
-    #     pb.print_progress_bar((i + 1) * 100 / num_samples)
-
-    #     if i in train_indexes:
-    #         dst_folder = 'data/train'
-    #     else:
-    #         dst_folder = 'data/valid'
-
-    #     dst_path = os.path.join(dst_folder, label)
-    #     if not os.path.exists(dst_path):
-    #         os.makedirs(dst_path)
-    #     dst_path = os.path.join(dst_path, fname)
-
-    #     crop_image = src_image[y1:y2, x1:x2]
-    #     dst_img = cv.resize(src=crop_image, dsize=(img_height, img_width))
-    #     cv.imwrite(dst_path, dst_img)
-
-
-def save_test_data(fnames, bboxes):
-    src_folder = 'cars_test'
-    dst_folder = 'data/test'
-    num_samples = len(fnames)
-
-    pb = ProgressBar(total=100, prefix='Save test data', suffix='', decimals=3, length=50, fill='=')
+    pb = ProgressBar(total=100, prefix='Save train and test data', suffix='', decimals=3, length=50, fill='=')
 
     for i in range(num_samples):
         fname = fnames[i]
-        (x1, y1, x2, y2) = bboxes[i]
-        src_path = os.path.join(src_folder, fname)
+        fnamePaste = fnamesPaste[i]
+        # print 'fname: ', fname
+        # print 'fnamePaste', fnamePaste
+        #label = labels[i]
+        #(x1, y1, x2, y2) = bboxes[i]
+
+        src_path = os.path.join(src_folder, fnamePaste)
         src_image = cv.imread(src_path)
-        height, width = src_image.shape[:2]
+        # print 'src_path: ', src_path
+        # print 'src_image: ', src_image
+        
+        #height, width = src_image.shape[:2]
+
         # margins of 16 pixels
-        margin = 16
-        x1 = max(0, x1 - margin)
-        y1 = max(0, y1 - margin)
-        x2 = min(x2 + margin, width)
-        y2 = min(y2 + margin, height)
-        # print(fname)
+        # margin = 16
+        # x1 = max(0, x1 - margin)
+        # y1 = max(0, y1 - margin)
+        # x2 = min(x2 + margin, width)
+        # y2 = min(y2 + margin, height)
+        # print("{} -> {}".format(fname, label))
         pb.print_progress_bar((i + 1) * 100 / num_samples)
 
-        dst_path = os.path.join(dst_folder, fname)
-        crop_image = src_image[y1:y2, x1:x2]
-        dst_img = cv.resize(src=crop_image, dsize=(img_height, img_width))
-        cv.imwrite(dst_path, dst_img)
+        if i in train_indexes:
+            dst_folder = 'data/train'
+        else:
+            dst_folder = 'data/test'
+
+
+        label = dict_labels[fnamePaste[:4]]
+        print 'label: ', label
+
+        dst_path = os.path.join(dst_folder, label)
+        if not os.path.exists(dst_path):
+            os.makedirs(dst_path)
+        dst_path = os.path.join(dst_path, fname)
+
+        # print 'src_image: ', src_image
+        # crop_image = src_image[y1:y2, x1:x2]
+        # dst_img = cv.resize(src=crop_image, dsize=(500, 500))
+        # print 'dst_img: ', dst_img
+        cv.imwrite(dst_path, src_image)
+
+# def save_test_data(fnames, bboxes):
+#     src_folder = 'cars_test'
+#     dst_folder = 'data/test'
+#     num_samples = len(fnames)
+
+#     pb = ProgressBar(total=100, prefix='Save test data', suffix='', decimals=3, length=50, fill='=')
+
+#     for i in range(num_samples):
+#         fname = fnames[i]
+#         (x1, y1, x2, y2) = bboxes[i]
+#         src_path = os.path.join(src_folder, fname)
+#         src_image = cv.imread(src_path)
+#         height, width = src_image.shape[:2]
+#         # margins of 16 pixels
+#         margin = 16
+#         x1 = max(0, x1 - margin)
+#         y1 = max(0, y1 - margin)
+#         x2 = min(x2 + margin, width)
+#         y2 = min(y2 + margin, height)
+#         # print(fname)
+#         pb.print_progress_bar((i + 1) * 100 / num_samples)
+
+#         dst_path = os.path.join(dst_folder, fname)
+#         crop_image = src_image[y1:y2, x1:x2]
+#         dst_img = cv.resize(src=crop_image, dsize=(img_height, img_width))
+#         cv.imwrite(dst_path, dst_img)
 
 
 # def process_train_data():
@@ -120,25 +142,25 @@ def save_test_data(fnames, bboxes):
 #     save_train_data(fnames, labels, bboxes)
 
 
-def process_test_data():
-    print("Processing test data...")
-    cars_annos = scipy.io.loadmat('devkit/cars_test_annos')
-    annotations = cars_annos['annotations']
-    annotations = np.transpose(annotations)
+# def process_test_data():
+#     print("Processing test data...")
+#     cars_annos = scipy.io.loadmat('devkit/cars_test_annos')
+#     annotations = cars_annos['annotations']
+#     annotations = np.transpose(annotations)
 
-    fnames = []
-    bboxes = []
+#     fnames = []
+#     bboxes = []
 
-    for annotation in annotations:
-        bbox_x1 = annotation[0][0][0][0]
-        bbox_y1 = annotation[0][1][0][0]
-        bbox_x2 = annotation[0][2][0][0]
-        bbox_y2 = annotation[0][3][0][0]
-        fname = annotation[0][4][0]
-        bboxes.append((bbox_x1, bbox_y1, bbox_x2, bbox_y2))
-        fnames.append(fname)
+#     for annotation in annotations:
+#         bbox_x1 = annotation[0][0][0][0]
+#         bbox_y1 = annotation[0][1][0][0]
+#         bbox_x2 = annotation[0][2][0][0]
+#         bbox_y2 = annotation[0][3][0][0]
+#         fname = annotation[0][4][0]
+#         bboxes.append((bbox_x1, bbox_y1, bbox_x2, bbox_y2))
+#         fnames.append(fname)
 
-    save_test_data(fnames, bboxes)
+#     save_test_data(fnames, bboxes)
 
 
 if __name__ == '__main__':
@@ -160,17 +182,51 @@ if __name__ == '__main__':
 
     #cars_meta = scipy.io.loadmat('devkit/cars_meta')
 
-    fnames = np.array(['Onix', 'HB20', 'Sand', 'VGol', 'Ford'])  # shape=(1, 5)
+    #fnames = np.array(['Onix', 'HB20', 'Sand', 'VGol', 'Ford'])  # shape=(1, 5)
     #class_names = np.transpose(class_names)
-    fnames.shape = (5, 1)
-    print('class_names.shape: ' + str(fnames.shape))
-    print('Sample class_name: [{}]'.format(fnames[3][0]))
+    #fnames.shape = (5, 1)
+    #print('class_names.shape: ' + str(fnames.shape))
+    #print('Sample class_name: [{}]'.format(fnames[3][0]))
+
+                    #chave : valor
+    dict_labels = { 
+                    'Onix' : '001', 
+                    'HB20' : '002',
+                    'Sand' : '003',
+                    'VGol' : '004',
+                    'Ford' : '005'
+                }
+
+    # #Apenas para testar se os valores e chaves do cicionário estão de acordo
+    # print 'dict_labels: ', dict_labels
+    # print 'keys: ', dict_labels.keys()
+    # for typeCar in dict_labels:
+    #     values = dict_labels[typeCar]
+    #     print 'values:', values
 
     ensure_folder('data/train')
-    ensure_folder('data/valid')
+    #ensure_folder('data/valid')
     ensure_folder('data/test')
 
-    save_train_data(fnames)
+    base_path = 'data/DeepLearningFilesPosAug'
+    paths = find_paths(base_path)
+
+    #Carrega os nomes das imagens em 'fnames'
+    fnames = []
+    fnamesPaste = []
+    for image_path in paths:
+        #print image_path
+        #im_data = load_image_data(image_path)
+        im_data = cv.imread(image_path)
+        fname = image_path[34:]
+        fnamePaste = image_path[29:]
+        #print 'fnamesPaste: ', fnamePaste
+        fnames.append(fname)
+        fnamesPaste.append(fnamePaste)
+
+    #print 'fnamesPaste: ', fnamesPaste
+
+    save_train_and_test_data(fnames, fnamesPaste, dict_labels)
 
     #process_train_data()
     #process_test_data()
